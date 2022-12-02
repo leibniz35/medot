ec5aa09dce12146ea330a09d95cb06e8ecabd452
leibniz35 <79407758+leibniz35@users.noreply.github.com>
2022-12-02 22:17:09 +0300

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from urllib.request import urlretrieve
 #st.image(filenamee)
 
 
-url = 'http://dl.dropboxusercontent.com/s/kcmjcwxjtxnpt5n/export.pkl?raw=1'
+url = 'http://dl.dropboxusercontent.com/s/wblnlkuldgxo5zw/export.pkl?raw=1'
 filename = 'export.pkl'
 urlretrieve(url,filename)
 st.markdown("<3")
